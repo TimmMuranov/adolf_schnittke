94228890af02ae307e099b1eb8ab470a49ed6ceb
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-04 18:07:56 +0300

    Add files via upload
    Исправил функцию (не я, а нейронка, но тем не менее теперь она работает

--- func/whataweek.h
+++ func/whataweek.h
@@ -2,21 +2,31 @@
 #include <string>
 #include <ctime>
 
-#pragma once
+// Определение констант
+const int FIRST_DAY = 33; // Первый день учебы в количестве дней от нового года
+const int FIRST_WEEK_PARITY = 0; // Четность первой недели в семестре. 1 - четная, 0 - нечетная
 
-#define FIRST_DAY  33 // Первый день учебы в к-ве дней от нового года
-#define FIRST_WEEK_PARITY 0 // Четность первой недели. 1 - четная, 0 - нечетная
-
-using namespace std;
-
-string whataweek(){
+std::string whatAWeek() {
     bool parityFlag = FIRST_WEEK_PARITY;
-    time_t t = time(NULL);
-    tm* timeinfo = localtime(&t);
+    
+    time_t t = time(nullptr);
+    struct tm* timeinfo = localtime(&t);
     int today = timeinfo->tm_yday;
 
-    if(((today - FIRST_DAY) / 7 ) % 2){
-        return "Сейчас ЧЕТНАЯ неделя.";
+    int weekNumber = (today - FIRST_DAY + 6) / 7;
+    if (weekNumber > 0) {
+        parityFlag = !parityFlag;
     }
-    return "Сейчас ЧЕТНАЯ неделя.";
+    
+    return parityFlag ? 
+       "Сейчас четная неделя." 
+       : //или
+       "Сейчас нечетная неделя.";
 }
+
+/*
+int main() {
+    std::cout << whatAWeek() << std::endl;
+    return 0;
+}
+*/
