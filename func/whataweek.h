d98c0e63faa485843ea2a22f274c8c402e859302
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-06 08:59:31 +0000

    докоммитил

--- func/whataweek.h
+++ func/whataweek.h
@@ -5,7 +5,7 @@
 const int FIRST_DAY = 33; // Первый день учебы в количестве дней от нового года
 const int FIRST_WEEK_PARITY = 0; // Четность первой недели в семестре. 1 - четная, 0 - нечетная
 
-std::string whatAWeek() {
+std::string whataweek() {
     bool parityFlag = FIRST_WEEK_PARITY;
     
     time_t t = time(nullptr);
@@ -13,7 +13,7 @@ std::string whatAWeek() {
     int today = timeinfo->tm_yday;
 
     int weekNumber = (today - FIRST_DAY + 6) / 7;
-    if (weekNumber > 0) {
+    if (weekNumber % 2 == 0) {
         parityFlag = !parityFlag;
     }
     
