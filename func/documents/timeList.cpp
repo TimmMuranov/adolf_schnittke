d687895a8a753cc9c7694f440fc747d0c07cc54d
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-08 10:56:25 +0000

    Добавил структуру дня

---
+++ func/documents/timeList.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <map>
+
+#pragma once
+
+struct day{
+    public:
+        std::vector<std::map<std::string, std::string>> events;
+        
+        void addEvent(std::string time, std::string event){
+
+        }
+        void removeEvent(std::string time){
+
+        }
+
+
+};
+
+
