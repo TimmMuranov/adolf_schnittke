ebf338e24e41475615a48f9b21c1ffc2f5a47859
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-08 12:01:04 +0000

    обновил структуру дня

--- func/documents/timeList.cpp
+++ func/documents/timeList.cpp
@@ -1,21 +1,18 @@
 #include <string>
-#include <vector>
 #include <map>
 
 #pragma once
 
 struct day{
     public:
-        std::vector<std::map<std::string, std::string>> events;
+        std::map<unsigned float[2], std::string> events;//Пример значений: 11.00, 12.30 - "Хор"
         
         void addEvent(std::string time, std::string event){
-
+            
         }
         void removeEvent(std::string time){
-
+            
         }
-
-
 };
 
 
