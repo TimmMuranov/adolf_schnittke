783450c40167d46cbd530c42a9ee7ee3faa8309d
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2025-02-08 16:02:44 +0000

    апгрейл недели

--- func/structs/week.hpp
+++ func/structs/week.hpp
@@ -1,4 +1,5 @@
 #include "day.hpp"
+#include <stdlib.h>
 
 struct Week:Day {
     public:
@@ -11,6 +12,20 @@ struct Week:Day {
             endl;
             std::string eventList;
             cin >> eventList;
+            int cond_flag = 0;
+            int marker_cord = 0;
+            for(int x=0; x<eventList.length(); ++x){
+                if(eventList[x] == '&'){
+                    if(eventList[x+1] == '&'){
+                        cond_flag = 0;
+                        x += 2;
+                    } else {
+                        string day = eventList.substr(marker_cord, x);
+                        int day = atoi(day.c_str());
+                        //дописать, я задрался уже....
+                    }
+                }
+            }
         }
     private:
 }
